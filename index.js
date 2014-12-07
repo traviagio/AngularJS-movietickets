d5f9fd437c4a5dfd0718509a5dab1711c0da9803
Georgi Georgiev <georgiev.georgio@gmail.com>
2014-12-07 15:39:50 +0000

    last step mods

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require("express"),
     fs = require('fs'),
-    json = require('express-json'),
+    bodyParser = require('body-parser'),
     port = process.env.PORT || 2595;
+
+var bookings = []; //fix2: created faka database
  
 var app = express();
 // app.use(express.logger());
-app.use(json());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false })) //fix1 - added url-encoding 
+
+// parse application/json
+app.use(bodyParser.json())
+
 // app.use(express.urlencoded());
 // app.set("view options", {
 //     layout: false
@@ -19,6 +27,10 @@ app.get('/', function (req, res) {
 app.get('/movies', function (req, res) {
     var movies = require('./data/movies.json');
     res.json(movies);
+});
+
+app.get('/bookings', function (req, res) {
+    res.json(bookings);
 });
 
 app.post('/book', function (req, res) {
--- public/js/router/router.js
+++ public/js/router/router.js
@@ -6,6 +6,9 @@ movieStubApp.config(function ($routeProvider) {
         }).when('/movie/:id', {
             templateUrl: 'tmpl/movie.html',
             controller: 'movieDetailsController'
+        }).when('/bookings', {
+            templateUrl: 'tmpl/bookings.html',
+            controller: 'bookingDetailsController'
         }).when('/bookTickets/:id', {
             templateUrl: 'tmpl/bookTickets.html',
             controller: 'bookTicketsController'
